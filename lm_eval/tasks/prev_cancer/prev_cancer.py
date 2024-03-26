b18c7a28338255528f7c30fcb2b1aeb8d1f44391
Zardar Khan <zardar.khan@icloud.com>
2024-03-26 09:04:50 -0400

    working example of prev_cancer

---
+++ lm_eval/tasks/prev_cancer/prev_cancer.py
@@ -0,0 +1,2 @@
+# from datasets import load_dataset
+# dataset = load_dataset("previousca_data.jsonl", split="validation", trust_remote_code=True)  # equivalent because the file has the same name as the directory
